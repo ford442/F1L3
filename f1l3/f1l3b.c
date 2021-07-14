46482467d6c2d16b862d341068f8683318e8c13a
Noah Cohn <noahc42@gmail.com>
2021-07-13 19:09:48 -0700

    Update f1l3b.c

--- f1l3/f1l3b.c
+++ f1l3/f1l3b.c
@@ -17,20 +17,20 @@ let disp=document.getElementById('display').innerHTML;
 fa.addEventListener('message',e => {
 document.getElementById('strt').click();
 
-var tex = ("document.getElementById("strt").addEventListener('click', function(){
+var tex = ("document.getElementById('strt').addEventListener('click', function(){
 <");
 var texb = ("?php
-$ffss = filesize("test.1ink.us/f1l3/test.txt");
-echo "let fsize=$ffss;";
+$ffss = filesize('test.1ink.us/f1l3/test.txt');
+echo 'let fsize=$ffss;';
 ?>
-document.getElementById("display").innerHTML=fsize;
+document.getElementById('display').innerHTML=fsize;
 <?php
-$url = "https://test.1ink.us/f1l3/test.txt";
+$url = 'https://test.1ink.us/f1l3/test.txt';
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_OPTIONS, true);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$headers = array("Access-Control-Request-Method: GET", "Access-Control-Request-Headers: range", "Range: bytes=0-92",);
+$headers = array('Access-Control-Request-Method: GET', 'Access-Control-Request-Headers: range', 'Range: bytes=0-92',);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); 
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -40,7 +40,7 @@ curl_close($curl);
 <");
 var texc = ("?php
 $resp = json_encode($resp);
-echo "var phpdat = $resp;";
+echo 'var phpdat = $resp;';
 ?>
 let W=new WebAssembly.Memory({initial:5});
 let wasmbuff=new Uint8ClampedArray(W.buffer,0,128); 
