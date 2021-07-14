22f1e43324731b55d2b5f90f8393de0cf3412a0b
Noah Cohn <noahc42@gmail.com>
2021-07-13 17:38:09 -0700

    Add files via upload

--- f1l3/f1l3d.c
+++ f1l3/f1l3d.c
@@ -14,9 +14,7 @@ let opts={type: 'idb',webWorkerSupport: false};
 const fa=new
 BroadcastChannel('f1a',opts);
 let disp=document.getElementById('display').innerHTML;
-fa.addEventListener('message',
-e=
-> {
+fa.addEventListener('message',e => {
 document.getElementById('strt').
 click();
 var fs=require("fs");
