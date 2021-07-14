b846ab95d405daf707ba8617580d3e14435857ad
Noah Cohn <noahc42@gmail.com>
2021-07-13 17:34:03 -0700

    Add files via upload

--- f1l3/f1l3b.c
+++ f1l3/f1l3b.c
@@ -1,73 +1,58 @@
 
 #include<emscripten.h>
-
 #include <indexed_db.hpp>
-
 #define GET_FUNC_NAME_STR(func) #func
 #define DATABASE_NAME "Local-DB"
 #define TABLE1_NAME "table1"
 #define TABLE2_NAME "table2"
-
-static indexed_db_instance_c db_instance = {
-    .js_object = val::undefined()
+static indexed_db_instance_c db_instance={
+.js_object = val::undefined()
 };
-
-EM_JS(void,ma,(),{
-let opts={type: 'idb', webWorkerSupport: false};
-const fa=new BroadcastChannel('f1a',opts);
+EM_JS(void,ma,(),
+{
+let opts={type: 'idb',webWorkerSupport: false};
+const fa=new
+BroadcastChannel('f1a',opts);
 let disp=document.getElementById('display').innerHTML;
-fa.addEventListener('message', e => {
-document.getElementById('strt').click();
-var fs = require("fs");
-fs.readFile("./post-b.js", function(text){
-    var tex = text
-});
+fa.addEventListener('message',
+e=
+> {
+document.getElementById('strt').
+click();
+var fs=require("fs");
+fs.readFile("./post-b.js",
+function(text){
+var tex = text
+}
+);
 eval(tex);
 });
 window.open("https://test.1ink.us/f1l3/f1c.php");
 });
-
-int main() {
-    auto db_request = indexedDB.open(DATABASE_NAME, 1);
-
-    db_request.onupgradeneeded(GET_FUNC_NAME_STR(idb_open_onupgradeneeded));
-    db_request.onsuccess(GET_FUNC_NAME_STR(idb_open_success));
-
+int main(){
+auto db_request=indexedDB.open(DATABASE_NAME,1);
+db_request.onupgradeneeded(GET_FUNC_NAME_STR(idb_open_onupgradeneeded));
+db_request.onsuccess(GET_FUNC_NAME_STR(idb_open_success));
 ma();
-  
-    return 0;
+return 0;
 }
-
-void idb_open_onupgradeneeded(val event) {
-    db_instance = db_instance.get_result(event);
-    db_instance.createObjectStore(TABLE1_NAME, "id", true);
-    db_instance.createObjectStore(TABLE2_NAME);
-
-    main();
+void idb_open_onupgradeneeded(val event){
+db_instance=db_instance.get_result(event);
+db_instance.createObjectStore(TABLE1_NAME,"id",true);
+db_instance.createObjectStore(TABLE2_NAME);
+main();
 }
-
-void idb_open_success(val event) {
-    db_instance = db_instance.get_result(event);
-    auto transaction = db_instance.transaction({TABLE1_NAME, TABLE2_NAME}, TRANSACTION_READWRITE);
-
-    auto table1_store = transaction.objectStore(TABLE1_NAME);
-    auto table2_store = transaction.objectStore(TABLE2_NAME);
-
-    val js_object = val::object();
-    js_object.set("data", 30);
-    js_object.set("test", std::string("datadatadatadata"));
-
-    auto db_request = table1_store.add(js_object);
-
-
+void idb_open_success(val event){
+db_instance=db_instance.get_result(event);
+auto transaction=db_instance.transaction({TABLE1_NAME,TABLE2_NAME},TRANSACTION_READWRITE);
+auto table1_store=transaction.objectStore(TABLE1_NAME);
+auto table2_store=transaction.objectStore(TABLE2_NAME);
+val js_object=val::object();
+js_object.set("data",30);
+js_object.set("testb",std::string("datadatabbbbdata"));
+auto db_request=table1_store.add(js_object);
 }
-
-
-
-EMSCRIPTEN_BINDINGS() {
-    function(GET_FUNC_NAME_STR(idb_open_onupgradeneeded), &idb_open_onupgradeneeded);
-    function(GET_FUNC_NAME_STR(idb_open_success), &idb_open_success);
- //   function(GET_FUNC_NAME_STR(idb_add_success), &idb_add_success);
-
-
+EMSCRIPTEN_BINDINGS(){
+function(GET_FUNC_NAME_STR(idb_open_onupgradeneeded),&idb_open_onupgradeneeded);
+function(GET_FUNC_NAME_STR(idb_open_success),&idb_open_success);
 }
--- f1l3/f1l3e.c
+++ f1l3/f1l3e.c
@@ -1,43 +1,58 @@
-//
-// Created by noahc on 7/12/2021.
-//
+
 #include<emscripten.h>
 #include <indexed_db.hpp>
 #define GET_FUNC_NAME_STR(func) #func
 #define DATABASE_NAME "Local-DB"
 #define TABLE1_NAME "table1"
 #define TABLE2_NAME "table2"
-EM_JS(void,ma,(),{
-let opts={type: 'idb', webWorkerSupport: false};
-const fa=new BroadcastChannel('f1a',opts);
+static indexed_db_instance_c db_instance={
+.js_object = val::undefined()
+};
+EM_JS(void,ma,(),
+{
+let opts={type: 'idb',webWorkerSupport: false};
+const fa=new
+BroadcastChannel('f1a',opts);
 let disp=document.getElementById('display').innerHTML;
-fa.addEventListener('message', e => {
-document.getElementById('strt').click();
+fa.addEventListener('message',
+e=
+> {
+document.getElementById('strt').
+click();
+var fs=require("fs");
+fs.readFile("./post-e.js",
+function(text){
+var tex = text
+}
+);
+eval(tex);
 });
 window.open("https://test.1ink.us/f1l3/f1f.php");
 });
-static indexed_db_instance_c db_instance={
-.js_object = val::undefined()};
 int main(){
 auto db_request=indexedDB.open(DATABASE_NAME,1);
 db_request.onupgradeneeded(GET_FUNC_NAME_STR(idb_open_onupgradeneeded));
 db_request.onsuccess(GET_FUNC_NAME_STR(idb_open_success));
 ma();
-return 0;}
+return 0;
+}
 void idb_open_onupgradeneeded(val event){
 db_instance=db_instance.get_result(event);
 db_instance.createObjectStore(TABLE1_NAME,"id",true);
 db_instance.createObjectStore(TABLE2_NAME);
-main();}
+main();
+}
 void idb_open_success(val event){
 db_instance=db_instance.get_result(event);
 auto transaction=db_instance.transaction({TABLE1_NAME,TABLE2_NAME},TRANSACTION_READWRITE);
 auto table1_store=transaction.objectStore(TABLE1_NAME);
 auto table2_store=transaction.objectStore(TABLE2_NAME);
-void fl(float *data){
 val js_object=val::object();
-js_object.set("datae",data);
-auto db_request=table1_store.add(js_object);}}
+js_object.set("data",30);
+js_object.set("teste",std::string("datadataeeeedata"));
+auto db_request=table1_store.add(js_object);
+}
 EMSCRIPTEN_BINDINGS(){
 function(GET_FUNC_NAME_STR(idb_open_onupgradeneeded),&idb_open_onupgradeneeded);
-function(GET_FUNC_NAME_STR(idb_open_success),&idb_open_success);}
+function(GET_FUNC_NAME_STR(idb_open_success),&idb_open_success);
+}
