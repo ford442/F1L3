0c56b936fa59bf275ea17749acb626e641d9ff8f
Noah Cohn <noahc42@gmail.com>
2021-07-22 14:06:19 -0700

    Update f1l3f.c

--- f1l3/f1l3f.c
+++ f1l3/f1l3f.c
@@ -18,13 +18,13 @@ let sbtn=document.getElementById('sbtn');
 sbtn.addEventListener("click",function(){
 /* let fileHandler= window.showSaveFilePicker({suggestedName: 'test.txt',types: [{description: 'TEXT',accept: {'text/txt': ['.txt'],},}],});
 */     
-let fdb;
+var fdb;
 var request = window.indexedDB.open("F1L3", 1);
 request.onsuccess = function(event) {
 fdb = request.result;
 var transaction = db.transaction(["parts"]);
 var objectStore = transaction.objectStore("parts");
-var request = objectStore.get("00");
+request = objectStore.get("00");
 request.onsuccess = function(event) {
 if(request.result) {
 console.log(request.result.data);
