3feedcefe33714655d2e93b9563cd288f523b038
Noah Cohn <noahc42@gmail.com>
2021-07-25 15:50:50 -0700

    Update f1l3f.c

--- f1l3/f1l3f.c
+++ f1l3/f1l3f.c
@@ -5,7 +5,6 @@ let fa=new BroadcastChannel('f1a',options);
 let ff=new BroadcastChannel('f1f',options);
 let bz=new BroadcastChannel('bez',options);
 bz.postMessage({data: 222});
-fa.postMessage({data: 222});
 ff.addEventListener('message',ef => {
 let f1a=new WebAssembly.Memory({initial:20});
 let w1=new Uint8ClampedArray(f1a.buffer,0,512);
@@ -26,6 +25,7 @@ document.getElementById('fbtn').addEventListener("click",function(){
 fa.postMessage({data: 888});
 });
 document.getElementById('sbtn').addEventListener("click",function(){
+fa.postMessage({data: 222});
 fa.postMessage({data: 666});
 });
 });
