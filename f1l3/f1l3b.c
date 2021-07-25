e3321e57a27987b236234c9b1d5c440711586522
Noah Cohn <noahc42@gmail.com>
2021-07-25 12:12:43 -0700

    Update f1l3b.c

--- f1l3/f1l3b.c
+++ f1l3/f1l3b.c
@@ -4,22 +4,21 @@
 #define DATABASE_NAME "F1L3"
 #define TABLE1_NAME "table1"
 #define TABLE2_NAME "table2"
-static indexed_db_instance_c db_instance={.js_object = val::undefined()};
 EM_JS(void,ma,(),{
-let opts={type: 'idb',webWorkerSupport: false};
-const fa=new BroadcastChannel('f1a',opts);
-let disp=document.getElementById('display').innerHTML;
-fa.addEventListener('message',e=> {
-document.getElementById('strt').
-click();
-});
 window.open("https://test.1ink.us/f1l3/f1c.php");
+let opts={type: 'idb',webWorkerSupport: false};
+const fa=new BroadcastChannel('strt',opts);
+fa.addEventListener('message',e=> {
+document.getElementById('strt').click();
 });
+});
+static indexed_db_instance_c db_instance={.js_object = val::undefined()};
 int main(){
+ma();
 auto db_request=indexedDB.open(DATABASE_NAME,1);
 db_request.onupgradeneeded(GET_FUNC_NAME_STR(idb_open_onupgradeneeded));
 db_request.onsuccess(GET_FUNC_NAME_STR(idb_open_success));
-ma();return 0;}
+return 0;}
 void idb_open_onupgradeneeded(val event){
 db_instance=db_instance.get_result(event);
 db_instance.createObjectStore(TABLE1_NAME,"part",false);
