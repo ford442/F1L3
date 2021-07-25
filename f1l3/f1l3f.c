6db093d394a03a6771769c8f1d13a9d417a47eb1
Noah Cohn <noahc42@gmail.com>
2021-07-25 15:57:42 -0700

    Update f1l3f.c

--- f1l3/f1l3f.c
+++ f1l3/f1l3f.c
@@ -4,7 +4,7 @@ let options={type: 'idb',webWorkerSupport: false};
 let fa=new BroadcastChannel('f1a',options);
 let ff=new BroadcastChannel('f1f',options);
 let bz=new BroadcastChannel('bez',options);
-bz.postMessage({data: 222});
+bz.postMessage({data: "222"});
 ff.addEventListener('message',ef => {
 let f1a=new WebAssembly.Memory({initial:20});
 let w1=new Uint8ClampedArray(f1a.buffer,0,512);
@@ -12,21 +12,21 @@ w1.set(ef.data,0);
 document.getElementById('disp').innerHTML=f1a.buffer;
 });
 fa.addEventListener('message',ea => {
-if (ea.data===667){
+if (ea.data="667"){
 console.log("Part A Saved.");}
-if (ea.data===668){
+if (ea.data="668"){
 console.log("Part B Saved.");}
-if (ea.data===669){
+if (ea.data="669"){
 console.log("Part C Saved.");}
-if (ea.data===670){
+if (ea.data="670"){
 console.log("Part D Saved.");}
 });
 document.getElementById('fbtn').addEventListener("click",function(){
-fa.postMessage({data: 888});
+fa.postMessage({data: "888"});
 });
 document.getElementById('sbtn').addEventListener("click",function(){
-fa.postMessage({data: 222});
-fa.postMessage({data: 666});
+fa.postMessage({data: "222"});
+fa.postMessage({data: "666"});
 });
 });
 int main(){ma();return 1;}
