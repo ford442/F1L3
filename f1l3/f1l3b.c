5b84b0bd0beab859c473e8824640892c5089b442
Noah Cohn <noahc42@gmail.com>
2021-07-27 00:44:52 -0700

    Update f1l3b.c

--- f1l3/f1l3b.c
+++ f1l3/f1l3b.c
@@ -7,8 +7,7 @@
 static indexed_db_instance_c db_instance={.js_object = val::undefined()};
 int main(){
 return 0;}
-int siz=128;
-int php00(int siz, int prtC[siz]){
+int php00(int prtC[]){
 int prt[siz] = prtC;
 return 0;}
 int php00st(){
@@ -21,7 +20,7 @@ db_instance=db_instance.get_result(event);
 db_instance.createObjectStore(TABLE1_NAME,"part",false);
 db_instance.createObjectStore(TABLE2_NAME);
 php00st();}
-void idb_open_success(val event,int prt){
+void idb_open_success(val event,int prt[]){
 db_instance=db_instance.get_result(event);
 auto transaction=db_instance.transaction({TABLE1_NAME,TABLE2_NAME},TRANSACTION_READWRITE);
 auto table1_store=transaction.objectStore(TABLE1_NAME);
