738717f03a466d57392d74753b9f4ad18869c859
Noah Cohn <noahc42@gmail.com>
2021-07-26 23:25:19 -0700

    Update f1l3b.c

--- f1l3/f1l3b.c
+++ f1l3/f1l3b.c
@@ -8,7 +8,7 @@ int prt[128];
 static indexed_db_instance_c db_instance={.js_object = val::undefined()};
 int main(){
 return 0;}
-void php00(int prt[128]);
+void php00(int prt[]);
 int php00st(){
 auto db_request=indexedDB.open(DATABASE_NAME,1);
 db_request.onupgradeneeded(GET_FUNC_NAME_STR(idb_open_onupgradeneeded));
