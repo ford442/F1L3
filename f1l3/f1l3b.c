84a2172f1b90f7f9776b7d9b0b6c189b737f17c2
Noah Cohn <noahc42@gmail.com>
2021-07-26 22:40:06 -0700

    Update f1l3b.c

--- f1l3/f1l3b.c
+++ f1l3/f1l3b.c
@@ -27,7 +27,7 @@ auto table1_store=transaction.objectStore(TABLE1_NAME);
 auto table2_store=transaction.objectStore(TABLE2_NAME);
 val js_object=val::object();
 js_object.set("part",std::string("$00"));
-js_object.set("data",int prt);
+js_object.set("data",(int prt));
 auto db_request=table1_store.add(js_object);}
 EMSCRIPTEN_BINDINGS(){
 function(GET_FUNC_NAME_STR(idb_open_onupgradeneeded),&idb_open_onupgradeneeded);
