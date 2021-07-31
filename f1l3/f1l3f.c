8d06e8292dcae0bd32c1c55c67af5c390e0e21bf
Noah Cohn <noahc42@gmail.com>
2021-07-31 13:59:30 -0700

    Update f1l3f.c

--- f1l3/f1l3f.c
+++ f1l3/f1l3f.c
@@ -11,16 +11,6 @@ let w1=new Uint8ClampedArray(f1a.buffer,0,512);
 w1.set(ef.data,0);
 document.getElementById('disp').innerHTML=f1a.buffer;
 });
-fa.addEventListener('message',ea => {
-if (ea.data="667"){
-console.log("Part A Saved.");}
-if (ea.data="668"){
-console.log("Part B Saved.");}
-if (ea.data="669"){
-console.log("Part C Saved.");}
-if (ea.data="670"){
-console.log("Part D Saved.");}
-});
 document.getElementById('fbtn').addEventListener("click",function(){
 fa.postMessage({data: "888"});
 });
