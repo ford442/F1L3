0977228839246244d250321f7030cc895e491d44
Noah Cohn <noahc42@gmail.com>
2021-11-27 00:42:19 -0800

    Update indexed_db.cpp

--- source/indexed_db.cpp
+++ source/indexed_db.cpp
@@ -1,9 +1,9 @@
 #include <indexed_db.hpp>
 indexed_db_request_c indexed_db_c::open(const char *name,unsigned version){
-return {.js_object = this->js_object.call<val>("open",std::string(name),version)};
+return{.js_object=this->js_object.call<val>("open",std::string(name),version)};
 }
 indexed_db_request_c indexed_db_c::deleteDatabase(const char *name){
-return {.js_object = this->js_object.call<val>("deleteDatabase",std::string(name))};
+return{.js_object=this->js_object.call<val>("deleteDatabase",std::string(name))};
 }
 void indexed_db_request_c::onsuccess(const char *callback_name){
 this->js_object.set("onsuccess",val::module_property(callback_name));
@@ -15,7 +15,7 @@ void indexed_db_request_c::onupgradeneeded(const char *callback_name){
 this->js_object.set("onupgradeneeded",val::module_property(callback_name));
 }
 indexed_db_instance_c indexed_db_instance_c::get_result(val event){
-return {.js_object = event["target"].as<val>()["result"].as<val>()};
+return {.js_object=event["target"].as<val>()["result"].as<val>()};
 }
 void indexed_db_instance_c::createObjectStore(const char *table_name){
 this->js_object.call<void>("createObjectStore",std::string(table_name));
@@ -30,14 +30,14 @@ indexed_db_transaction_c
 indexed_db_instance_c::transaction(std::vector <std::string> tables,transaction_type_t transaction_type){
 val js_tables=val::array(tables);
 static const std::string transaction_types_str[]={"readonly","readwrite"};
-return {.js_object = this->js_object.call<val>("transaction",js_tables,transaction_types_str[transaction_type])};
+return {.js_object=this->js_object.call<val>("transaction",js_tables,transaction_types_str[transaction_type])};
 }
 indexed_db_object_store_c indexed_db_transaction_c::objectStore(const char *table_name){
-return {.js_object = this->js_object.call<val>("objectStore",std::string(table_name))};
+return {.js_object=this->js_object.call<val>("objectStore",std::string(table_name))};
 }
 indexed_db_request_c indexed_db_object_store_c::add(val object){
-return {.js_object = this->js_object.call<val>("add",object)};
+return {.js_object=this->js_object.call<val>("add",object)};
 }
 indexed_db_request_c indexed_db_object_store_c::add(val object,int key){
-return {.js_object = this->js_object.call<val>("add",object,key)};
+return {.js_object=this->js_object.call<val>("add",object,key)};
 }
