1b1b52e3766d4b5a1fad125e23e7ec2cd962a9da
Noah Cohn <noahc42@gmail.com>
2021-12-13 20:16:21 -0800

    Update iconv.sh

--- build/public_html/iconv.sh
+++ build/public_html/iconv.sh
@@ -1,5 +1,4 @@
 
-
 mkdir u16
 
 iconv -f UTF-8 -t UTF-16 1ink.1ink > ./u16/1ink.1ink
@@ -16,17 +15,17 @@ iconv -f UTF-8 -t UTF-16 f1e.php > ./u16/f1e.1ink
 
 iconv -f UTF-8 -t UTF-16 f1f.php > ./u16/f1f.1ink
 
-iconv -f UTF-8 -t UTF-16 fa.js > ./u16/fa.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/fa.js > ./u16/fa.1ijs
 
-iconv -f UTF-8 -t UTF-16 fb.js > ./u16/fb.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/fb.js > ./u16/fb.1ijs
 
-iconv -f UTF-8 -t UTF-16 fc.js > ./u16/fc.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/fc.js > ./u16/fc.1ijs
 
-iconv -f UTF-8 -t UTF-16 fd.js > ./u16/fd.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/fd.js > ./u16/fd.1ijs
 
-iconv -f UTF-8 -t UTF-16 fe.js > ./u16/fe.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/fe.js > ./u16/fe.1ijs
 
-iconv -f UTF-8 -t UTF-16 ff.js > ./u16/ff.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/ff.js > ./u16/ff.1ijs
 
 iconv -f UTF-8 -t UTF-16 post-b.php > ./u16/post-b.1ink
 
@@ -36,10 +35,12 @@ iconv -f UTF-8 -t UTF-16 post-d.php > ./u16/post-d.1ink
 
 iconv -f UTF-8 -t UTF-16 post-e.php > ./u16/post-e.1ink
 
-iconv -f UTF-8 -t UTF-16 post-b.js > ./u16/post-b.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/post-b.js > ./u16/post-b.1ijs
 
-iconv -f UTF-8 -t UTF-16 post-c.js > ./u16/post-c.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/post-c.js > ./u16/post-c.1ijs
 
-iconv -f UTF-8 -t UTF-16 post-d.js > ./u16/post-d.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/post-d.js > ./u16/post-d.1ijs
 
-iconv -f UTF-8 -t UTF-16 post-e.js > ./u16/post-e.1ijs
+iconv -f UTF-8 -t UTF-16 ./js/post-e.js > ./u16/post-e.1ijs
+
+iconv -f UTF-8 -t UTF-16 post-f.js > ./u16/post-e.1ijs
