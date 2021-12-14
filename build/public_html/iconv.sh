c8b8a739acb69ed708a9cf5c7d6d125ffa1a76e3
Noah Cohn <noahc42@gmail.com>
2021-12-13 20:13:43 -0800

    Create iconv.sh

---
+++ build/public_html/iconv.sh
@@ -0,0 +1,45 @@
+
+
+mkdir u16
+
+iconv -f UTF-8 -t UTF-16 1ink.1ink > ./u16/1ink.1ink
+
+iconv -f UTF-8 -t UTF-16 bezz.htm > ./u16/bezz.1ink
+
+iconv -f UTF-8 -t UTF-16 f1b.php > ./u16/f1b.1ink
+
+iconv -f UTF-8 -t UTF-16 f1c.php > ./u16/f1c.1ink
+
+iconv -f UTF-8 -t UTF-16 f1d.php > ./u16/f1d.1ink
+
+iconv -f UTF-8 -t UTF-16 f1e.php > ./u16/f1e.1ink
+
+iconv -f UTF-8 -t UTF-16 f1f.php > ./u16/f1f.1ink
+
+iconv -f UTF-8 -t UTF-16 fa.js > ./u16/fa.1ijs
+
+iconv -f UTF-8 -t UTF-16 fb.js > ./u16/fb.1ijs
+
+iconv -f UTF-8 -t UTF-16 fc.js > ./u16/fc.1ijs
+
+iconv -f UTF-8 -t UTF-16 fd.js > ./u16/fd.1ijs
+
+iconv -f UTF-8 -t UTF-16 fe.js > ./u16/fe.1ijs
+
+iconv -f UTF-8 -t UTF-16 ff.js > ./u16/ff.1ijs
+
+iconv -f UTF-8 -t UTF-16 post-b.php > ./u16/post-b.1ink
+
+iconv -f UTF-8 -t UTF-16 post-c.php > ./u16/post-c.1ink
+
+iconv -f UTF-8 -t UTF-16 post-d.php > ./u16/post-d.1ink
+
+iconv -f UTF-8 -t UTF-16 post-e.php > ./u16/post-e.1ink
+
+iconv -f UTF-8 -t UTF-16 post-b.js > ./u16/post-b.1ijs
+
+iconv -f UTF-8 -t UTF-16 post-c.js > ./u16/post-c.1ijs
+
+iconv -f UTF-8 -t UTF-16 post-d.js > ./u16/post-d.1ijs
+
+iconv -f UTF-8 -t UTF-16 post-e.js > ./u16/post-e.1ijs
