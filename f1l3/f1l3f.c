ea111abdd5e927c4c6fcb16e401b53984dd21deb
Noah Cohn <noahc42@gmail.com>
2021-12-13 21:06:30 -0800

    Update f1l3f.c

--- f1l3/f1l3f.c
+++ f1l3/f1l3f.c
@@ -8,16 +8,11 @@
 #include <GLES3/gl3.h>
 #include <emscripten.h>
 #include <emscripten/html5.h>
-#include <SDL2/SDL.h>
 #include <iostream>
 #include <ctime>
 
 using namespace std;
 using namespace std::chrono;
-
-Uint8 *stm;
-SDL_AudioDeviceID dev;
-struct{SDL_AudioSpec spec;Uint8* snd;Uint32 slen;int pos;}wave;
 
 using std::string;
 steady_clock::time_point t1;
@@ -286,60 +281,12 @@ emscripten_set_main_loop((void (*)())renderFrame,0,0);
 }
 static void cls_aud(){
 if(dev!=0){
-SDL_PauseAudioDevice(dev,SDL_TRUE);
-SDL_CloseAudioDevice(dev);
-dev=0;
-}}
-static void qu(int rc){
-SDL_Quit();
-exit(rc);
-}
-static void opn_aud(){
-dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
-if(!dev){
-SDL_FreeWAV(wave.snd);
-qu(2);
-}
-SDL_PauseAudioDevice(dev,SDL_FALSE);
-}
-Uint8 *wptr;
-int lft;
-static void SDLCALL bfr(void *unused,Uint8 *stm,int len){
-wptr=wave.snd+wave.pos;
-lft=wave.slen-wave.pos;
-while (lft<=len){
-SDL_memcpy(stm,wptr,lft);
-stm+=lft;
-len-=lft;
-wptr=wave.snd;
-lft=wave.slen;
-wave.pos=0;
-}
-SDL_memcpy(stm,wptr,len);
-wave.pos+=len;
-}
-char flnm[16];
-static void plt(){
-SDL_SetMainReady();
-if (SDL_Init(SDL_INIT_AUDIO)<0){
-qu(1);
-SDL_Log("SDL failed to init.");
-}
-SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
-if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){
-qu(1);
-}
-wave.pos=0;
-wave.spec.callback=bfr;
-opn_aud();
-}
+SDL_PauseAudioDev
 extern "C" {
 void str(){
 strt();
 }
-void pl(){
-plt();
-}}
+}
 
 EM_JS(void,ma,(),{
 let options={type: 'idb',webWorkerSupport: false};
