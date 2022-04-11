7c853b2f5f877096f3352a4777be210a3ad6c895
Noah <noahc42@gmail.com>
2022-04-10 22:11:53 -0700

    add url button

--- f1l3/post-b.js
+++ f1l3/post-b.js
@@ -1,6 +1,7 @@
 document.getElementById("strt").addEventListener('click',function(){
-<?php
-$url="https://test.1ink.us/f1l3/test.txt";
+    var filo=document.getElementById("filoc").innerHTML;
+    <?php
+    $url="<script>document.write(filo)</script>";
 $curl=curl_init($url);
 curl_setopt($curl,CURLOPT_URL,$url);
 curl_setopt($curl,CURLOPT_OPTIONS,true);
