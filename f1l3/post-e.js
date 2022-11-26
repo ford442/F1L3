58d1c7d9bbd38d9468f2123a1000610f44e318a2
Noah Cohn <noahc42@gmail.com>
2022-11-26 12:55:31 -0800

    Update post-e.js

--- f1l3/post-e.js
+++ f1l3/post-e.js
@@ -1,28 +1,19 @@
 document.getElementById("strt").addEventListener('click',function(){
-    var filo=document.getElementById("filoc").innerHTML;
-    <?php
-    $url="<script>document.write(filo)</script>";
-$curl=curl_init($url);
-curl_setopt($curl,CURLOPT_URL,$url);
-curl_setopt($curl,CURLOPT_OPTIONS,true);
-curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-$headers=array("Access-Control-Request-Method: GET", "Access-Control-Request-Headers: range", "Range: bytes=279-374",);
-curl_setopt($curl,CURLOPT_HTTPHEADER,$headers);
-curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,false); 
-curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
-$resp=curl_exec($curl);
-curl_close($curl);
-?>
-<?php
-$resp=json_encode($resp);
-echo "var phpdat=$resp;";
-?>
+let pth=document.getElementById("filoc").innerHTML;
+let ff=new XMLHttpRequest();
+ff.open("GET",pth,true);
+ff.responseType="arraybuffer";
+ff.onload=function(oEvent){
+let sarrayBuffer=ff.response;
+if(sarrayBuffer){
 let W=new WebAssembly.Memory({initial:5});
 let wasmbuff=new Uint8ClampedArray(W.buffer,0,128); 
 const textEncoder=new TextEncoder();
-let encoded=textEncoder.encode(phpdat);
+let encoded=textEncoder.encode(sarrayBuffer);
 encoded=new Uint8ClampedArray(encoded);
 wasmbuff.set(encoded,0);
 let fill=Module.cwrap('fl',null,['array'])
 fill(w.buffer);
+}}
+ff.send(null);
 });
