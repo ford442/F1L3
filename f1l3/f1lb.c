aa166fda832cc4cac3e896ba9fbf1775c47edd8f
Noah Cohn <noahc42@gmail.com>
2022-11-26 12:18:46 -0800

    Update f1lb.c

--- f1l3/f1lb.c
+++ f1l3/f1lb.c
@@ -1,10 +1,13 @@
-#include<emscripten.h>
+#include <emscripten.h>
 #include <indexed_db.hpp>
+
 #define GET_FUNC_NAME_STR(func) #func
 #define DATABASE_NAME "Local-DB"
 #define TABLE1_NAME "table1"
 #define TABLE2_NAME "table2"
+
 extern "C" {
+  
 void fl(float *data){
 auto transaction=db_instance.transaction({TABLE1_NAME,TABLE2_NAME},TRANSACTION_READWRITE);
 auto table1_store=transaction.objectStore(TABLE1_NAME);
@@ -12,7 +15,9 @@ val js_object=val::object();
 js_object.set("datac",data);
 auto db_request=table1_store.add(js_object);
 }
+  
 }
+
 EM_JS(void,ma,(),{
 let opts={type: 'idb', webWorkerSupport: false};
 const fa=new BroadcastChannel('f1a',opts);
@@ -22,25 +27,33 @@ document.getElementById('strt').click();
 });
 window.open("https://test.1ink.us/f1l3/f1c.1ink");
 });
+
 int data;
-static indexed_db_instance_c db_instance={
-.js_object = val::undefined()};
+static indexed_db_instance_c db_instance={.js_object = val::undefined()};
+
 int main(){
 auto db_request=indexedDB.open(DATABASE_NAME,1);
 db_request.onupgradeneeded(GET_FUNC_NAME_STR(idb_open_onupgradeneeded));
 db_request.onsuccess(GET_FUNC_NAME_STR(idb_open_success));
 ma();
-return 0;}
+return 0;
+}
+
 void idb_open_onupgradeneeded(val event){
 db_instance=db_instance.get_result(event);
 db_instance.createObjectStore(TABLE1_NAME,"id",true);
 db_instance.createObjectStore(TABLE2_NAME);
-main();}
+main();
+}
+
 void idb_open_success(val event){
 db_instance=db_instance.get_result(event);
 auto transaction=db_instance.transaction({TABLE1_NAME,TABLE2_NAME},TRANSACTION_READWRITE);
 auto table1_store=transaction.objectStore(TABLE1_NAME);
-auto table2_store=transaction.objectStore(TABLE2_NAME);}
+auto table2_store=transaction.objectStore(TABLE2_NAME);
+}
+
 EMSCRIPTEN_BINDINGS(){
 function(GET_FUNC_NAME_STR(idb_open_onupgradeneeded),&idb_open_onupgradeneeded);
-function(GET_FUNC_NAME_STR(idb_open_success),&idb_open_success);}
+function(GET_FUNC_NAME_STR(idb_open_success),&idb_open_success);
+}
