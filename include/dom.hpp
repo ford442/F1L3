d12dd90f8aef57776abb839358bee3c9905feefa
Noah Cohn <noahc42@gmail.com>
2022-11-26 13:02:27 -0800

    Update dom.hpp

--- include/dom.hpp
+++ include/dom.hpp
@@ -2,7 +2,9 @@
 #include <vector>
 #include <emscripten/val.h>
 #include <emscripten/bind.h>
+
 using namespace emscripten;
+
 class dom_object_c{
 public:
 val js_object;
@@ -16,6 +18,7 @@ void classNamef(const char *class_string,...);
 dom_object_c querySelectorf(const char *selector_string,...);
 std::vector <dom_object_c> querySelectorAllf(const char *selector_string,...);
 };
+
 class dom_document_c{
 private:
 val js_object=val::global("document");
@@ -25,4 +28,5 @@ std::vector <dom_object_c> querySelectorAll(const char *selector_string);
 dom_object_c querySelectorf(const char *selector_string,...);
 std::vector <dom_object_c> querySelectorAllf(const char *selector_string,...);
 };
+
 static dom_document_c document;
