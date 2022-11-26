c54ff5c4205552d8c070551d59c8938bc7f00906
Noah Cohn <noahc42@gmail.com>
2022-11-26 13:13:15 -0800

    Update post-f.js

--- build/public_html/post-f.js
+++ build/public_html/post-f.js
@@ -1,13 +1,17 @@
 let db;
+
 let request=window.indexedDB.open("F1L3",1);
+
 request.onsuccess=function(event){
 db=request.result;
 };
+
 request.onupgradeneeded=function(event){
 db=event.target.result;
 let objectStore=db.createObjectStore("file",{keyPath:"part"});
 objectStore.add(fileData);
 }
+
 function read(){
 let transaction=db.transaction(["file"]);
 let objectStore=transaction.objectStore("file");
