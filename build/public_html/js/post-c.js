3c538ac7237bae5ddb488b422e3c85fac4a0689a
Noah Cohn <noahc42@gmail.com>
2022-11-26 13:25:04 -0800

    Update post-c.js

--- build/public_html/js/post-c.js
+++ build/public_html/js/post-c.js
@@ -1,29 +1,21 @@
-document.getElementById("strt").addEventListener('click', function(){
-<?php $ffss = filesize("https://test.1ink.us/f1l3/test.txt"); echo "let fsize=$ffss;"?>
-document.getElementById('display').innerHTML=fsize;
-<?php
-$url = "https://test.1ink.us/f1l3/test.txt";
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_OPTIONS, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$headers = array("Access-Control-Request-Method: GET", "Access-Control-Request-Headers: range", "Range: bytes=93-185",);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); 
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-$resp = curl_exec($curl);
-curl_close($curl);
-?>
-<?php
-$resp = json_encode($resp);
-echo "var phpdat = $resp;";
-?>
+document.getElementById("strt").addEventListener('click',function(){
+let pth=document.getElementById("filoc").innerHTML;
+let ff=new XMLHttpRequest();
+ff.open("GET",pth,true);
+ff.setRequestHeader('Range', 'bytes=93-185');
+
+ff.responseType="arraybuffer";
+ff.onload=function(oEvent){
+let sarrayBuffer=ff.response;
+if(sarrayBuffer){
 let W=new WebAssembly.Memory({initial:5});
 let wasmbuff=new Uint8ClampedArray(W.buffer,0,128); 
-const textEncoder = new TextEncoder();
-let encoded = textEncoder.encode(phpdat);
+const textEncoder=new TextEncoder();
+let encoded=textEncoder.encode(sarrayBuffer);
 encoded=new Uint8ClampedArray(encoded);
 wasmbuff.set(encoded,0);
-let fill = Module.cwrap('fl', null, ['array'])
+let fill=Module.cwrap('fl',null,['array'])
 fill(w.buffer);
+}}
+ff.send(null);
 });
