e9d7c2e9827fd9d9818cd3a2bbc1c297740f0543
Noah Cohn <noahc42@gmail.com>
2022-11-26 13:04:56 -0800

    Update console.cpp

--- source/console.cpp
+++ source/console.cpp
@@ -1,18 +1,22 @@
 #include <console.hpp>
 #include <stdarg.h>
+
 #ifdef WJS_DEBUG
 #include <stdlib.h>
 #endif
+
 #include "vsnprintf.cpp"
-enum{
-CONSOLE_SPRINTF_BUFFER_SIZE=512,
-};
+
+enum{CONSOLE_SPRINTF_BUFFER_SIZE=512,};
+
 void console_c::clear(){
 this->js_object.call<void>("clear");
 }
+
 void console_c::table(const val js_object){
 this->js_object.call<void>("table",js_object);
 }
+
 void console_print(const val js_object,const char *type,const char *string,va_list &arg_list){
 char sprintf_buffer[CONSOLE_SPRINTF_BUFFER_SIZE];
 std::vector <val> console_js_val;
@@ -39,6 +43,7 @@ js_object.call<void>(type,console_strings[0],console_js_val[0],
 console_strings[1],console_js_val[1],console_strings[2]);
 }
 break;
+    
 #ifdef WJS_DEBUG
 default: {
 snprintf(sprintf_buffer, sizeof(sprintf_buffer), "Console: Too many specifiers %%js in the string '%s'"
@@ -46,76 +51,90 @@ snprintf(sprintf_buffer, sizeof(sprintf_buffer), "Console: Too many specifiers %
 js_object.call<void>("error", std::string(sprintf_buffer));
 } exit(0);
 #endif
+    
 };
 }
+
 void console_c::log(const char *string,...){
 va_list arg_list;
 va_start(arg_list,string);
 console_print(this->js_object,"log",string,arg_list);
 va_end(arg_list);
 }
+
 void console_c::info(const char *string,...){
 va_list arg_list;
 va_start(arg_list,string);
 console_print(this->js_object,"info",string,arg_list);
 va_end(arg_list);
 }
+
 void console_c::warn(const char *string,...){
 va_list arg_list;
 va_start(arg_list,string);
 console_print(this->js_object,"warn",string,arg_list);
 va_end(arg_list);
 }
+
 void console_c::error(const char *string,...){
 va_list arg_list;
 va_start(arg_list,string);
 console_print(this->js_object,"error",string,arg_list);
 va_end(arg_list);
 }
+
 void console_call(const val js_object,const char *type,const char *label,va_list &arg_list){
 char sprintf_buffer[CONSOLE_SPRINTF_BUFFER_SIZE];
 console_sp::vsnprintf(sprintf_buffer,sizeof(sprintf_buffer),label,arg_list,(val **) {},(const char **) {});
 js_object.call<void>(type,std::string(sprintf_buffer));
 }
+
 void console_c::count(const char *label,...){
 va_list arg_list;
 va_start(arg_list,label);
 console_call(this->js_object,"count",label,arg_list);
 va_end(arg_list);
 }
+
 void console_c::countReset(const char *label,...){
 va_list arg_list;
 va_start(arg_list,label);
 console_call(this->js_object,"countReset",label,arg_list);
 va_end(arg_list);
 }
+
 void console_c::group(const char *label,...){
 va_list arg_list;
 va_start(arg_list,label);
 console_call(this->js_object,"group",label,arg_list);
 va_end(arg_list);
 }
+
 void console_c::groupCollapsed(const char *label,...){
 va_list arg_list;
 va_start(arg_list,label);
 console_call(this->js_object,"groupCollapsed",label,arg_list);
 va_end(arg_list);
 }
+
 void console_c::groupEnd(){
 this->js_object.call<void>("groupEnd");
 }
+
 void console_c::time(const char *label,...){
 va_list arg_list;
 va_start(arg_list,label);
 console_call(this->js_object,"time",label,arg_list);
 va_end(arg_list);
 }
+
 void console_c::timeLog(const char *label,...){
 va_list arg_list;
 va_start(arg_list,label);
 console_call(this->js_object,"timeLog",label,arg_list);
 va_end(arg_list);
 }
+
 void console_c::timeEnd(const char *label,...){
 va_list arg_list;
 va_start(arg_list,label);
