9883e1cd7bf4bda05f55be9769c26c3ca85e8d09
Noah Cohn <noahc42@gmail.com>
2022-11-26 17:30:07 -0800

    Update f1d.php

--- build/public_html/f1d.php
+++ build/public_html/f1d.php
@@ -101,6 +101,6 @@ document.getElementById("iwid").innerHTML=$w;
 document.getElementById("wrap").style.lineheight=$hg;
 document.getElementById("wrap").style.height=$hg;
 </script>
-<script charset="UTF-8" async src="./f1d.js"></script>
+<script charset="UTF-8" async src="./fd.1ijs"></script>
 <script src="./post-d.php"></script>
 </body>
