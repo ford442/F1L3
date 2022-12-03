f0973bd2b0f329ef504202b42a7b757b8754920e
Noah Cohn <noahc42@gmail.com>
2022-12-02 21:57:59 -0800

    Update post-e.js

--- f1l3/post-e.js
+++ f1l3/post-e.js
@@ -2,9 +2,7 @@ document.getElementById("strt").addEventListener('click',function(){
 let pth=document.getElementById("filoc").innerHTML;
 let ff=new XMLHttpRequest();
 ff.open("GET",pth,true);
-
-ff.setRequestHeader('Range', 'bytes=279-374');
-
+ff.setRequestHeader('Range','bytes=279-374');
 ff.responseType="arraybuffer";
 ff.onload=function(oEvent){
 let sarrayBuffer=ff.response;
